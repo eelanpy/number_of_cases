6cb0f2fcba6f334f4380ebc3c3d741bdb82a4711
Eelan Kisokkannan <eelank@Kisokkannans-iMac.local>
2021-03-27 18:13:02 -0400

    added new files

---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,48 @@
+import React from 'react'
+// import axios from 'axios'
+
+// const result =  axios(
+//     `https://xdg5krknz9.execute-api.us-east-2.amazonaws.com/test/schools?requester=eelan`
+// )
+
+  
+// console.log(result.data)
+
+
+
+
+
+// console.log(result.data)
+
+const CharacterItem = ({ item }) => {
+  console.log(item);
+  return (
+    <div className='card'>
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img src={item.img} alt=''  />
+        </div>
+        <div className={item.schoolRating}>
+          
+          <h3>{item.schoolName}</h3>
+          <ul>
+            <li>
+              <strong></strong> 
+            </li>
+            <li>
+              <strong>Number of cases:</strong> {item.cases}
+            </li>
+            <li>
+              <strong>Status:</strong> {item.status}
+            </li>
+            {/* <li>
+              <strong>Status again:</strong> {item.img}
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CharacterItem
---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//  
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={`https://cms.accuweather.com/wp-content/uploads/2020/03/CoronavirusAnimation2.gif?w=475`} alt='' />
+    </header>
+  )
+}
+
+export default Header
