14228a65a9e14f35f4e9bca2c0aef7a04cc272df
eelanpy <76450488+eelanpy@users.noreply.github.com>
2021-03-27 21:11:19 -0400

    added title (h1 tag)

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header className='center'>
       <img src={`https://cms.accuweather.com/wp-content/uploads/2020/03/CoronavirusAnimation2.gif?w=475`} alt='' />
+      <h1>Covid Cases at YRDSB</h1>
     </header>
   )
 }
